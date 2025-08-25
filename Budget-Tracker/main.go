16a9566a99d0531cdf1b7466737746b034a6693c
Alper Seyman <alperseyman@gmail.com>
2025-08-25 22:37:59 +0200

    budget tracker completed

--- Budget-Tracker/main.go
+++ Budget-Tracker/main.go
@@ -1,1 +1,77 @@
 package main
+
+import (
+	"fmt"
+
+	"github.com/AlperSeyman/budget-tracker/model"
+)
+
+func main() {
+
+	bt := &model.BudgetTracker{
+		NextID: 1,
+	}
+
+	for {
+		fmt.Println("\n--- Personal Budget Tracker---")
+		fmt.Println("1. Add Transaction")
+		fmt.Println("2. Display Transaction")
+		fmt.Println("3. Show Total Income")
+		fmt.Println("4. Show Total Expenses")
+		fmt.Println("5. Save Transaction to CSV")
+		fmt.Println("6. Exit")
+		fmt.Print("Choose an option: ")
+
+		var choice int
+		fmt.Scanln(&choice)
+
+		switch choice {
+		case 1:
+
+			fmt.Print("Enter Amount: ")
+			var amount float64
+			fmt.Scanln(&amount)
+
+			fmt.Print("Enter Category: ")
+			var category string
+			fmt.Scanln(&category)
+
+			fmt.Print("Enter type (Income/Expense): ")
+			var tType string
+			fmt.Scanln(&tType)
+
+			bt.AddTransaction(amount, category, tType)
+			fmt.Println("Transaction Added!")
+
+		case 2:
+
+			bt.DisplayTransactions()
+
+		case 3:
+
+			fmt.Printf("Total Income: %.2f\n", bt.CalculateTotalAmount("income"))
+
+		case 4:
+
+			fmt.Printf("Total Expenses: %.2f\n", bt.CalculateTotalAmount("expense"))
+
+		case 5:
+
+			fmt.Println("Enter filename (e.g. transactions.csv): ")
+			var filename string
+			fmt.Scanln(&filename)
+			if err := bt.SaveToCSV(filename); err != nil {
+				fmt.Println("Error saving transactions :", err)
+			}
+
+		case 6:
+
+			fmt.Println("Exiting....")
+			return
+
+		default:
+			fmt.Println("Invalid Choice! Try Again")
+
+		}
+	}
+}
--- Budget-Tracker/model/model.go
+++ Budget-Tracker/model/model.go
@@ -1,7 +1,10 @@
 package model
 
 import (
+	"encoding/csv"
 	"fmt"
+	"os"
+	"strconv"
 	"time"
 )
 
@@ -15,7 +18,7 @@ type Transaction struct {
 
 type BudgetTracker struct {
 	transactions []Transaction
-	nextID       int
+	NextID       int
 }
 
 type FinancialRecord interface {
@@ -34,7 +37,7 @@ func (t Transaction) GetType() string {
 func (bt *BudgetTracker) AddTransaction(amount float64, category string, tType string) {
 
 	newTransaction := Transaction{
-		ID:       bt.nextID,
+		ID:       bt.NextID,
 		Amount:   amount,
 		Category: category,
 		Date:     time.Now(),
@@ -42,7 +45,7 @@ func (bt *BudgetTracker) AddTransaction(amount float64, category string, tType s
 	}
 
 	bt.transactions = append(bt.transactions, newTransaction)
-	bt.nextID++
+	bt.NextID++
 }
 
 func (bt BudgetTracker) DisplayTransactions() {
@@ -50,6 +53,51 @@ func (bt BudgetTracker) DisplayTransactions() {
 	fmt.Println("ID\tAmount\tCategory\tDate\tType")
 
 	for _, transaction := range bt.transactions {
-		fmt.Printf("%d\t%.2f\t%s\t%s\t%s\n", transaction.ID, transaction.Amount, transaction.Category, transaction.Date, transaction.Type)
+		fmt.Printf("%d\t%.2f\t%s\t%s\t%s\n", transaction.ID, transaction.Amount, transaction.Category, transaction.Date.Format("2006-01-02"), transaction.Type)
 	}
+}
+
+func (bt BudgetTracker) CalculateTotalAmount(tType string) float64 {
+
+	var total float64
+
+	for _, transaction := range bt.transactions {
+		if transaction.Type == tType {
+			total = transaction.Amount + total
+		}
+	}
+	return total
+
+}
+
+func (bt BudgetTracker) SaveToCSV(filename string) error {
+
+	file, err := os.Create(filename) // create file
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file) // creating a new CSV file
+	defer writer.Flush()
+
+	writer.Write([]string{"ID", "Amount", "Category", "Date", "Type"}) // write the CSV header
+
+	// Write Data
+	for _, transaction := range bt.transactions {
+
+		record := []string{
+			strconv.Itoa(transaction.ID),
+			fmt.Sprintf("%.2f", transaction.Amount),
+			transaction.Category,
+			transaction.Date.Format("2006-01-02"),
+			transaction.Type,
+		}
+
+		writer.Write(record)
+
+	}
+	fmt.Println("Transactions saved to", filename)
+	return nil
+
 }
